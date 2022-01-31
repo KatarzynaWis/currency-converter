c6700f6f2ddf46ccd49a062f65c6e3567b336d46
Katarzyna Wiśniewska <katwis1b@gmail.com>
2022-01-31 19:48:49 +0100

    added options

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,16 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", () => {
-   // helper functions
-   const qs = (selector) => document.querySelector(selector);
+  // helper functions
+  const qs = (selector) => document.querySelector(selector);
 
-   // DOM elements
-   const imgChangingFlagDOM = qs(".background__circle-img-changing");
-   const inputAmoutDOM = qs("#amount");
-   const selectFromDOM = qs("#from");
-   const paragraphResultToConvertDOM = qs("#resultToConvert");
-   const paragraphResultConvertedDOM = qs("#resultConverted");
+  // DOM elements
+  const imgChangingFlagDOM = qs(".background__circle-img-changing");
+  const inputAmoutDOM = qs("#amount");
+  const spanSymbolDOM = qs("#symbol");
+  const selectFromDOM = qs("#from");
+  const paragraphResultToConvertDOM = qs("#resultToConvert");
+  const paragraphResultConvertedDOM = qs("#resultConverted");
 
   // data
   const emoji = {
@@ -19,18 +20,22 @@ window.addEventListener("DOMContentLoaded", () => {
     CHF: "&#x1F1E8&#x1F1ED",
     GBP: "&#x1F1EC&#x1F1E7",
   };
+  const symbols = {
+    euro: "&#x20AC",
+    dolar: "&#x24",
+    funt: "&#xA3",
+    frank: "CHF",
+  };
 
   // fetching data from the server
   fetch("http://api.nbp.pl/api/exchangerates/tables/a/?format=json")
     .then((response) => response.json())
     .then((response) => {
-      console.log(response);
-      console.log(response[0].rates);
       const rates = response[0].rates;
-      // cerate currency object
 
+      // cerate currency object
       const cerateCurrency = () => {
-        rates.map((obj, id, arr) => {
+        rates.map((obj) => {
           if (
             obj.code === "USD" ||
             obj.code === "NZD" ||
@@ -41,50 +46,47 @@ window.addEventListener("DOMContentLoaded", () => {
             const optionFromDOM = document.createElement("option");
             optionFromDOM.setAttribute("name", obj.currency);
             optionFromDOM.setAttribute("code", obj.code);
-            optionFromDOM.setAttribute("midRate", obj.mid);
+            optionFromDOM.setAttribute("mid", obj.mid);
 
             const findEmoji = () => {
-              const m = Object.entries(emoji).forEach((table) => {
-                if (table[0] === obj.code) {
-                  const emoji = table[1];
-                  return emoji;
-                }
-              })
-
-              return m;
+              const emojiInArray = Object.entries(emoji).filter(
+                (table) => table[0] === obj.code
+              );
+              const emojiString = emojiInArray.flat()[1];
+              return emojiString;
             };
-console.log(findEmoji());
-            optionFromDOM.textContent = `${findEmoji()} &nbsp; ${obj.code} - ${obj.currency}`;
+
+            const nameToUpperCase =
+              obj.currency.charAt(0).toUpperCase() + obj.currency.slice(1);
+            optionFromDOM.innerHTML = `${findEmoji()} &nbsp; &nbsp; &nbsp; ${
+              obj.code
+            } - ${nameToUpperCase}`;
+
             selectFromDOM.appendChild(optionFromDOM);
           }
         });
       };
       cerateCurrency();
+
+      const changeFlagAndSymbol = () => {
+        selectFromDOM.addEventListener("change", (e) => {
+          const codeAttritubeValue =
+            e.target.options[e.target.selectedIndex].attributes.code.value;
+          const nameAttritubeValue =
+            e.target.options[e.target.selectedIndex].attributes.name.value;
+        
+          imgChangingFlagDOM.src = `./images/${codeAttritubeValue}.jpg`;
+
+          const findSymbol = () => {
+            const symbolInArray = Object.entries(symbols).filter((table) => nameAttritubeValue.includes(table[0]));
+            const symbol = symbolInArray.flat()[1];
+            return symbol;
+          };
+          spanSymbolDOM.innerHTML = findSymbol();
+        });
+      };
+      changeFlagAndSymbol();
     })
-    .catch((error) => console.error(error));
-
- 
-
-  // data cd
-  const currencies = [];
-  const symbols = {
-    euro: "&#x20AC",
-    dollar: "&#x24",
-    pound: "&#xA3",
-    swissFranc: "CHF",
-  };
-
-  // data update
-  const addNewCurrency = (oldData, newCurrency) => {
-    return [...oldData, newCurrency];
-  };
-
-  const changeFlagAndSymbol = () => {
-    selectFromDOM.addEventListener("change", (e) => {
-      const optionValue = e.target.value;
-      imgChangingFlagDOM.src = `./images/${optionValue}.jpg`;
-      console.log(e.target.options[e.target.selectedIndex]);
-    });
-  };
-  changeFlagAndSymbol();
+    .catch((error) => console.error(error))
+    .finally();
 });
